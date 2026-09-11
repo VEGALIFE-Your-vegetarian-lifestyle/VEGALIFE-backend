efc3f9c486bbad07322f0181e4328898f2c6d3e7
zuyzz <duyldreal@gmail.com>
2026-09-11 09:00:32 +0700

    Maven init

---
+++ src/test/java/com/vegalife/VegalifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegalife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegalifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
